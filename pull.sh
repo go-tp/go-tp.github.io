6723028b0f01a2d5fdbfbf124607cb26f2a292df
gouzi <530971494@qq.com>
2022-09-26 22:03:58 +0800

    [script]: rm script

--- pull.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-/usr/bin/git checkout . && /usr/bin/git fetch origin main && /usr/bin/git reset --hard origin/main && /usr/bin/git pull -u origin main
-
-
-echo "go-tp update"
--- push.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-begin=`date +'%Y-%m-%d %H:%M:%y'`
-
-git add .
-git commit -m"$begin"
-git push -u origin main 
